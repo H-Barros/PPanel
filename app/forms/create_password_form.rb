5e0ba6050e8f8c09db539d2c6039dbe1a209205e
Hundov <fillipe.bugman.palhares@gmail.com>
2022-03-18 22:50:16 -0300

    Merge branch 'main' of https://github.com/H-Barros/PPanel into main

---
+++ app/forms/create_password_form.rb
@@ -0,0 +1,22 @@
+class CreatePasswordForm
+  include ActiveModel::Model
+
+  attr_accessor :preferential, :sector, :service
+
+  validates :preferential, inclusion: [true, false]
+  validates :sector, :service, presence: true
+
+  def initialize(sector, service, preferential = false)
+    @sector = sector
+    @service = service
+    @preferential = preferential
+  end
+
+  def save
+    return false if invalid?
+
+    password = Password.new(sector: @sector, service: @service, preferential: @preferential)
+    password.password_number_generator
+    password.save
+  end
+end
