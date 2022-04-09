bae1dc2ec50d1293d33342d17b3b20906a27c0f9
Henrique Barros <henriqueeustaquiob@gmail.com>
2022-04-09 13:02:49 -0300

    Configure sessions paths

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
     protected
-  
-    def after_sign_up_path_for(resource)
-        "/users/sign_in"
-    end
 
-    def after_sign_out_path_for(resource)
-        "/"
-      end
-  end
+    def after_sign_up_path_for(resource)
+        new_user_session_path
+    end
+end
